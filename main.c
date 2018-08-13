22aba9af01b2e4a4b3a335ed40e56089847bd13e
Justus Perlwitz <mail@justusperlwitz.com>
2018-08-13 20:40:53 +0900

    Main: Clean up test code

--- main.c
+++ main.c
@@ -11,15 +11,10 @@ void kernel_main() {
   init_gdt();
   init_idt();
   screen_clear();
-  for (int i = 0; i < 20; i++) {
-    screen_puts("This is a test!\n");
-  }
   screen_printf("Hello, %s\n", "World");
   screen_printf("The answer is %4d\n", 42);
   screen_printf("And in hexadecimal, 255 + 1 : %02x + %02x = %04x\n", 255, 1,
                 255 + 1);
-  asm volatile("int $0x3");
-  asm volatile("int $0x4");
 
   init_timer(1);
   for (;;) {
