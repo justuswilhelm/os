9acfc55fb98df8e133b01749fa9b00a50de91132
Justus Perlwitz <mail@justusperlwitz.com>
2020-10-30 12:04:26 +0900

    Main: Printing test fn

--- main.c
+++ main.c
@@ -7,15 +7,20 @@
 #include "screen.h"
 #include "timer.h"
 
+void test_print() {
+  screen_printf("Hello, %s\n", "World");
+  screen_printf("The answer is %4d\n", 42);
+  screen_printf("Printing negative numbers! %04d\n", -42);
+  screen_printf("And in hexadecimal, 255 + 1 : %02x + %02x = %04x\n", 255, 1,
+                255 + 1);
+}
+
 // void kernel_main(struct multiboot *mboot_ptr) {
 void kernel_main() {
   init_gdt();
   init_idt();
   screen_clear();
-  screen_printf("Hello, %s\n", "World");
-  screen_printf("The answer is %4d\n", 42);
-  screen_printf("And in hexadecimal, 255 + 1 : %02x + %02x = %04x\n", 255, 1,
-                255 + 1);
+  test_print();
 
   // init_timer(1);
   init_keyboard();
