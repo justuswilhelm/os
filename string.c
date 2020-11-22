97e8917ea907161b91d3250145bb676d20004bd9
Justus Perlwitz <mail@justusperlwitz.com>
2020-11-22 11:55:07 +0900

    String: Add memcpy, strncmp

--- string.c
+++ string.c
@@ -13,3 +13,21 @@ void *memset(void *dest, int ch, size_t count) {
 
   return dest;
 }
+
+int strncmp(const char *lhs, const char *rhs, size_t num) {
+  for (size_t i = 0; i < num; i++) {
+    if (lhs[i] != rhs[i]) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+void *memcpy(void *dest, const void *src, size_t num) {
+  char *_dest = dest;
+  const char *_src = src;
+  for (size_t i = 0; i < num; i++) {
+    _dest[i] = _src[i];
+  }
+  return dest;
+}
--- string.h
+++ string.h
@@ -1,3 +1,6 @@
+#include <stdbool.h>
 #include <stddef.h>
 
 void *memset(void *, int, size_t);
+void *memcpy(void *, const void *, size_t);
+int strncmp(const char *, const char *, size_t);
